c4e45477e887f7a76c241b853cc01051c9296d62
NPitroff <nicolai.pitroff@gmail.com>
2022-01-15 08:50:26 -0800

    Added a JS file

---
+++ script.js
@@ -0,0 +1,6 @@
+// query selector for number display
+let numberDisplay = document.querySelector(".display");
+// query selector to add
+let addNumber = document.querySelector(".add");
+// query selector to subtract
+let subNumber = document.querySelector(".sub");
